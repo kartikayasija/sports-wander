43cd66b7a27381fdaa531908e5e481cd37f841d6
Kartikay Asija <kartikay7057@gmail.com>
2023-04-29 16:09:38 +0530

    organizers section added

---
+++ script.js
@@ -0,0 +1,5 @@
+const showDropdown = ()=>{
+  var dropdown = document.querySelector(".nav-mob");
+  if(dropdown.style.display=="block") dropdown.style.display="none";
+  else dropdown.style.display="block";
+}
