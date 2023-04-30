70855164c8dbe76edf091ee59da9d0a356284c7b
Kartikay Asija <kartikay7057@gmail.com>
2023-04-30 17:46:44 +0530

    responsiveness improved

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const showDropdown = ()=>{
   var dropdown = document.querySelector(".nav-mob");
-  if(dropdown.style.display=="block") dropdown.style.display="none";
-  else dropdown.style.display="block";
+  if(dropdown.style.display=="block"){
+    dropdown.style.display="none";
+    dropdown.style.padding="0px";
+  }else {
+    dropdown.style.display="block";
+    dropdown.style.padding="5px";
+  }
 }
